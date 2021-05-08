b1b791fd331e1bbade246ba478d7c4ce5c5a6183
Lee-ji-soo <nbsp3@naver.com>
2021-05-08 10:37:33 +0900

    nodejs server connect

---
+++ back/server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const mysql = require("mysql");
+const api = require("./api");
+const cors = require("cors");
+
+dotenv.config();
+const PORT = process.env.PORT || 80;
+
+const app = express();
+let conn;
+let sql;
+
+app.use(cors({
+  origin:["http://localhost:3000"],
+  credentials: true
+}))
+
+app.use("/api", api);
+
+app.listen(PORT, ()=>{
+  console.log(`💚 START EXPRESS SERVER : PORT NUMBER : ${PORT}`)
+})
