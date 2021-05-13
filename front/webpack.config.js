9c2c06c92d26ef9c4f07e4f0b1c5484f2b7a0b60
Sooleeandtomas <62638990+Lee-ji-soo@users.noreply.github.com>
2021-05-13 13:47:54 +0900

    package setting

--- front/webpack.config.js
+++ front/webpack.config.js
@@ -16,7 +16,7 @@ module.exports = {
         },
       },
       {
-        test: /\.s[ac]ss$/i,
+        test: /\.(css|s[ac]ss)$/i,
         use: ["style-loader", "css-loader", "sass-loader"],
       },
       {
