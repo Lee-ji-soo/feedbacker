48a44802659ef0f9d358869eae07ef1217d423f8
Sooleeandtomas <62638990+Lee-ji-soo@users.noreply.github.com>
2021-05-15 00:19:07 +0900

    delte back project

--- back/routers/auth.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const authController = require("../controllers/auth");
-
-router.post("/login", authController.login);
-router.get("/loggedIn", authController.loggedIn);
-
-module.exports = router;
