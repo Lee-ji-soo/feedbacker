002d3904362e8d4388ef8befd8e2d4fd12875723
Sooleeandtomas <62638990+Lee-ji-soo@users.noreply.github.com>
2021-05-19 00:25:24 +0900

    meta tag

--- front/src/pages/FeedList.js
+++ front/src/pages/FeedList.js
@@ -3,6 +3,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { Grid } from "../elements";
 import { Feed } from "../components";
 import { actionCreators as postAction } from "../redux/modules/post";
+import { Helmet } from "react-helmet";
 
 const FeedList = (props) => {
   const dispatch = useDispatch();
@@ -14,12 +15,20 @@ const FeedList = (props) => {
   }, []);
 
   return (
-    <Grid padding="30px 0">
-      {feed_list.map((feed, i) => {
-        const isMe = user_info && feed.user_info.user_id === user_info.uid
-          return <Feed key={`FEED_${i}`} {...feed} isMe={isMe}/>
-      })}
-    </Grid>
+    <div>
+      <Helmet>
+        <title>feedbacker</title>
+        <meta property="og:title" content="og page feedbacker"/>
+        <meta property="og:description" content="feed for designer"/>
+        <meta property="og:image" content=""/>
+      </Helmet>
+      <Grid padding="30px 0">
+        {feed_list.map((feed, i) => {
+          const isMe = user_info && feed.user_info.user_id === user_info.uid
+            return <Feed key={`FEED_${i}`} {...feed} isMe={isMe}/>
+        })}
+      </Grid>
+    </div>
   );
 };
 
